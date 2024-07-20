a844b2f8928de35a30e181a0c264c47e94cb10e5
Marek Schir <schir2@msn.com>
2024-07-19 20:27:05 -0400

    finished diagonal validation logic began on word region combination logic

--- generator/board_generator.py
+++ generator/board_generator.py
@@ -1,7 +1,8 @@
 import copy
+from collections import defaultdict
 from itertools import combinations
 
-from spangram_placement_strategies import SpangramPlacementStrategy, RightToLeftStrategy
+from generator.spangram_placement_strategies import SpangramPlacementStrategy, RightToLeftStrategy
 
 
 class InvalidRegionLengthException(Exception):
@@ -15,7 +16,8 @@ class BoardGenerator:
     words: list[str] = None
     spangram: str = None
     valid_boards: list = []
-    df = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    orthogonal_changes = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    diagonal_changes = {(1, 1), (1, -1), (-1, 1), (-1, -1)}
     spangram_placement_strategy: SpangramPlacementStrategy = None
 
     def __init__(self, rows: int, cols: int, words: list[str], spangram: str,
@@ -28,70 +30,88 @@ class BoardGenerator:
         self.spangram_placement_strategy = spangram_placement_strategy_class(rows=rows, cols=cols)
         self.region_length_combinations = self.generate_region_length_combinations(words)
 
-    def generate_board(self, spangram: str, words: list) -> list[list[str]]:
-        """
-        :param spangram:
-        :param words:
-
-        1. Spangram will split the board so on both ends the board must have a sufficient amount of tiles to cover the rest of the words.
-        """
-
     @staticmethod
     def generate_region_length_combinations(words):
         # TODO This needs to generate the actual combinations of words
+        words = set(words)
         total_length = sum(len(word) for word in words)
-        region_length_combinations = set()
+        combinations_map = defaultdict(set)
 
         for num_words in range(1, len(words)):
             for combo in combinations(words, num_words):
                 combo_length = sum(len(word) for word in combo)
                 complementary_length = total_length - combo_length
                 if combo_length > 0 and complementary_length > 0:
-                    region_length_combinations.add((combo_length, complementary_length))
-                    region_length_combinations.add((complementary_length, combo_length))
-
-        return region_length_combinations
+                    combo_tuple = tuple(sorted(combo))
+                    combo_complement_tuple = tuple(sorted(words - set(combo)))
+                    combinations_map[(combo_length, complementary_length)].add((combo_tuple, combo_complement_tuple))
+                    combinations_map[(complementary_length, combo_length)].add((combo_complement_tuple, combo_tuple))
+        return combinations_map
 
     def generate_spangram_boards(self):
         starting_positions = self.spangram_placement_strategy.starting_positions()
         for starting_row, starting_col in starting_positions:
-            self.generate_spangram_board(starting_row, starting_col, 0, [(starting_row, starting_col)], set(),
-                                         self.create_empty_board())
+            self.generate_spangram_board(
+                row=starting_row,
+                col=starting_col,
+                i=0,
+                path=[(starting_row, starting_col)],
+                vertices=set(),
+                board=self.create_empty_board(self.rows, self.cols)
+            )
         return self.valid_boards
 
-    def create_empty_board(self) -> list[list[str | None]]:
-        board = [[None for _ in range(self.cols)] for _ in range(self.rows)]
+    @staticmethod
+    def create_empty_board(rows: int, cols: int) -> list[list[str | None]]:
+        board = [[None for _ in range(cols)] for _ in range(rows)]
         return board
 
-    def crossed_diagonally(self, row: int, col: int) -> bool:
+    @classmethod
+    def crossed_diagonally(cls, vertex: list[tuple[int, int]], vertices: set) -> bool:
+        complimentary_vertices = cls.get_complimentary_vertices(vertex)
+        if not complimentary_vertices:
+            return False
+        for complimentary_vertex in complimentary_vertices:
+            if complimentary_vertex in vertices:
+                return True
         return False
 
-    def generate_spangram_board(self, row: int, col: int, i: int, path: list[tuple[int, int]],
-                                vertices: set[tuple[tuple[int, int], tuple[int, int]]],
-                                board: list[list[str]]) -> bool:
+    @classmethod
+    def get_complimentary_vertices(cls, vertex) -> tuple | None:
+        if len(vertex) < 2:
+            return
+        df = (vertex[0][0] - vertex[1][0], vertex[0][1] - vertex[1][1])
+        if df not in cls.diagonal_changes:
+            return
+        complimentary_vertex = ((vertex[0][0] - df[0], vertex[0][1] + 0), (vertex[0][0] + 0, vertex[0][1] - df[1]))
+        complimentary_vertices = (complimentary_vertex, (complimentary_vertex[1], complimentary_vertex[0]))
+        return complimentary_vertices
+
+    def generate_spangram_board(self, row: int, col: int, i: int, path: list, vertices: set, board: list) -> bool:
         if not (0 <= row < self.rows and 0 <= col < self.cols):
             return False
         if board[row][col]:
             return False
 
-        board[row][col] = self.spangram[i]
         distance_from_end = self.spangram_placement_strategy.distance_form_end(*path[-1])
         letters_to_go = len(self.spangram) - i - 1
 
         if distance_from_end > letters_to_go:
-            board[row][col] = None
             return False
+
+        if len(path) > 1:
+            vertices.add((path[-2], path[-1]))
+
+        if self.crossed_diagonally(path[-2:], vertices):
+            return False
+
+        board[row][col] = self.spangram[i]
+
         if letters_to_go == 0 and distance_from_end == 0:
             board_tuple = copy.deepcopy(board)
             self.valid_boards.append(board_tuple)
             board[row][col] = None
             return True
-        if self.crossed_diagonally(row=row, col=col):
-            board[row][col] = None
-            return False
-
-        if len(path) > 1:
-            vertices.add((path[-2], path[-1]))
 
         directions = []
 
@@ -129,12 +149,12 @@ class BoardGenerator:
             return
         board[row][col] = ''
         regions[-1].add((row, col))
-        for dr, dc in cls.df:
+        for dr, dc in cls.orthogonal_changes:
             cls._create_region(row=row + dr, col=col + dc, board=board, regions=regions)
 
-    def validate_spangram_board(cls, board):
+    def validate_spangram_board(self, board):
         try:
-            unfilled_regions = cls.get_unfilled_regions(board)
+            unfilled_regions = self.get_unfilled_regions(board)
         except InvalidRegionLengthException:
             return False
 
@@ -144,12 +164,6 @@ class BoardGenerator:
 if __name__ == '__main__':
     results = []
     for strategy_class in (RightToLeftStrategy,):
-        generator = BoardGenerator(4, 4, ['flap', 'test', 'brown'], 'test', strategy_class)
+        generator = BoardGenerator(2, 2, ['flap', 'test', 'brown'], 'test', strategy_class)
         boards = generator.generate_spangram_boards()
-        for board in boards:
-            for row in board:
-                print(row)
-            print(10 * '_')
-            unfilled_regions = generator.validate_spangram_board(board)
-            print(unfilled_regions)
-            print(10 * '_')
+        results.append(boards)
